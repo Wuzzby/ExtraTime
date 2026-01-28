ad02460eea78dc07b1eed89c2b390fb2fc1337aa
Sam Achek <samachek@proton.me>
2026-01-27 19:15:23 -0500

    Fix match detail page JSX return block

--- app/match/[id]/page.tsx
+++ app/match/[id]/page.tsx
@@ -2,8 +2,10 @@ import { notFound } from "next/navigation";
 import { getMatchById } from "@/lib/sampleData";
 import MatchHeader from "@/components/MatchHeader";
 import RatingBlock from "@/components/RatingBlock";
+import RateMatchForm from "@/components/RateMatchForm";
 import ReviewList from "@/components/ReviewList";
 import ExternalLinks from "@/components/ExternalLinks";
+
 
 type PageProps = {
   params: Promise<{ id: string }>;
@@ -21,9 +23,10 @@ export default async function MatchDetailPage({ params }: PageProps) {
 
       <hr style={{ margin: "1.5rem 0" }} />
 
-      <RatingBlock />
-      <ReviewList />
+      <RatingBlock matchId={match.id} />
+      <RateMatchForm matchId={match.id} />
+      <ReviewList matchId={match.id} />
       <ExternalLinks matchId={match.id} />
-    </>
+   </>
   );
 }
